02abeecd676619e6ad1672e64e335366318bcbad
numankh <khannuman15@gmail.com>
2019-09-01 11:14:12 -0400

    integrated serverless and user exists functionality

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
-import hashlib
-import create_db, update_db
+import json
 import pymysql
+import update_db
 import utilities
 
 #------------------------------Part1--------------------------------
@@ -8,19 +8,27 @@ import utilities
 # a dictionary with company news
 Company_LIST = ["Apple", "Amazon", "Microsoft"]
 Company_NEWS = {"apple":"This is apple news", "amazon":"This is amazon news", "microsoft":"This is microsoft news"}
+host="stockvoicedb.cwnolqf8w7br.us-east-1.rds.amazonaws.com"
+port=3306
+dbname="testDB"
+user="admin"
+password="khan2931"
+
+mydb = pymysql.connect(port=port, host=host, user=user, passwd=password, database=dbname)
+mycursor = mydb.cursor()
 
 #------------------------------Part2--------------------------------
-# Here we define our Lambda function and configure what it does when
-# an event with a Launch, Intent and Session End Requests are sent. # The Lambda function responses to an event carrying a particular
-# Request are handled by functions such as on_launch(event) and
+# Here we define our Lambda function and configure what it does when 
+# an event with a Launch, Intent and Session End Requests are sent. # The Lambda function responses to an event carrying a particular 
+# Request are handled by functions such as on_launch(event) and 
 # intent_scheme(event).
 def lambda_handler(event, context):
     # connecting to database
-    conn = pymysql.connect(dbhost, user=dbuser,port=dbport,
-                           passwd=dbpassword, db=dbname)
-    cursor = conn.cursor()
-
-    # checking the request type
+    
+    # mycursor.execute("show tables;")
+    # for x in mycursor:
+    #     print(x)
+    
     if event['session']['new']:
         on_start()
     if event['request']['type'] == "LaunchRequest":
@@ -29,7 +37,7 @@ def lambda_handler(event, context):
         return intent_scheme(event)
     elif event['request']['type'] == "SessionEndedRequest":
         return on_end()
-
+        
 #------------------------------Part3--------------------------------
 # Here we define the Request handler functions
 def on_start():
@@ -41,40 +49,41 @@ def on_launch(event):
     reprompt_MSG = "Do you want to hear more about a particular company?"
     card_TEXT = "Pick a company."
     card_TITLE = "Choose a company."
-
-    # Processing userId to see if it exists in the database
-    userIdStr = event['context']['System']['user']['userId']
-    userIdStr = hashID(userIdStr)
-    userExists(userIdStr, cursor)
     
+    userId = event['context']['System']['user']['userId']
+    userId = utilities.hashID(userId)
+    appId = event['context']['System']['application']['applicationId']
+    deviceId = event['context']['System']['device']['deviceId']
+    
+    # print(update_db.userExists(userId, mycursor))
+    
+    if not update_db.userExists(userId, mycursor):
+        update_db.addUser(mydb, mycursor, userId, appId, deviceId)
+        print("IS THIS WORKING")
+        
     return output_json_builder_with_reprompt_and_card(onlunch_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
 
 def on_end():
     print("Session Ended.")
 #-----------------------------Part3.1-------------------------------
-# The intent_scheme(event) function handles the Intent Request.
-# Since we have a few different intents in our skill, we need to
-# configure what this function will do upon receiving a particular
-# intent. This can be done by introducing the functions which handle
+# The intent_scheme(event) function handles the Intent Request. 
+# Since we have a few different intents in our skill, we need to 
+# configure what this function will do upon receiving a particular 
+# intent. This can be done by introducing the functions which handle 
 # each of the intents.
 def intent_scheme(event):
-    print("THIS IS THE USER ID")
-    print(event['context']['System']['user']['userId'])
-    userIdStr = event['context']['System']['user']['userId']
-    encodedID = int(hashlib.sha256(userIdStr.encode('utf-8')).hexdigest(), 16) % 10**8
-    print(encodedID)
-
+    
     intent_name = event['request']['intent']['name']
 
     if intent_name == "GetCompanyNews":
-        return player_bio(event)
+        return player_bio(event)        
     elif intent_name in ["AMAZON.NoIntent", "AMAZON.StopIntent", "AMAZON.CancelIntent"]:
         return stop_the_skill(event)
     elif intent_name == "AMAZON.HelpIntent":
         return assistance(event)
     elif intent_name == "AMAZON.FallbackIntent":
         return fallback_call(event)
-
+        
 #---------------------------Part3.1.1-------------------------------
 # Here we define the intent handler functions
 def player_bio(event):
@@ -91,14 +100,14 @@ def player_bio(event):
         card_TEXT = "Use the full name."
         card_TITLE = "Wrong name."
         return output_json_builder_with_reprompt_and_card(wrongname_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
-
+        
 def stop_the_skill(event):
     stop_MSG = "Thank you. Bye!"
     reprompt_MSG = ""
     card_TEXT = "Bye."
     card_TITLE = "Bye Bye."
     return output_json_builder_with_reprompt_and_card(stop_MSG, card_TEXT, card_TITLE, reprompt_MSG, True)
-
+    
 def assistance(event):
     assistance_MSG = "You can choose among these companies: " + ', '.join(map(str, Company_LIST)) + ". Be sure to use the full name when asking about the company."
     reprompt_MSG = "Do you want to hear more about a particular company?"
@@ -113,10 +122,10 @@ def fallback_call(event):
     card_TITLE = "Wrong question."
     return output_json_builder_with_reprompt_and_card(fallback_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
 #------------------------------Part4--------------------------------
-# The response of our Lambda function should be in a json format.
-# That is why in this part of the code we define the functions which
+# The response of our Lambda function should be in a json format. 
+# That is why in this part of the code we define the functions which 
 # will build the response in the requested format. These functions
-# are used by both the intent handlers and the request handlers to
+# are used by both the intent handlers and the request handlers to 
 # build the output.
 def plain_text_builder(text_body):
     text_dict = {}
@@ -128,13 +137,13 @@ def reprompt_builder(repr_text):
     reprompt_dict = {}
     reprompt_dict['outputSpeech'] = plain_text_builder(repr_text)
     return reprompt_dict
-
+    
 def card_builder(c_text, c_title):
     card_dict = {}
     card_dict['type'] = "Simple"
     card_dict['title'] = c_title
     card_dict['content'] = c_text
-    return card_dict
+    return card_dict    
 
 def response_field_builder_with_reprompt_and_card(outputSpeach_text, card_text, card_title, reprompt_text, value):
     speech_dict = {}
--- update_db.py
+++ update_db.py
@@ -1,14 +1,16 @@
 def userExists(id, cursor):
-    cursor.execute("SELECT * FROM master where userId={}".format(id))
+    cursor.execute("SELECT * FROM master where userId={};".format(id))
     myresult = cursor.fetchall()
 
     if(len(myresult) == 0):
         print("NEW USER: adding to database")
-        addUser(id, cursor)
+        return False
     else:
         print("EXISTING USER :)")
+        return True
 
-def addUser(id, cursor):
+def addUser(conn, cursor, userId, appId, deviceId):
     sql = "INSERT INTO master (userId, appId, deviceId) VALUES (%s, %s, %s)"
-    val = (id, "blah", "blah")
+    val = (userId, appId, deviceId)
     cursor.execute(sql, val)
+    conn.commit()
